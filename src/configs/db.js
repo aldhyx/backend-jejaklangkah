f2d78cda8be4caad0329357ec398f6357854cdaa
Frialdhy Ketty <aldyzkobf@gmail.com>
2020-10-10 18:40:11 +0800

    done config

--- src/configs/db.js
+++ src/configs/db.js
@@ -5,6 +5,7 @@ const dbConnection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
+  port: process.eventNames.DB_PORT,
   multipleStatements: true,
 });
 
